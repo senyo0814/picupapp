6b404024a8b457359ef010682242d1941f2195bb
senyo0814 <chingescat@gmail.com>
2025-05-04 22:39:48 +0000

    add logo

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,7 +77,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]  # for development
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # for collectstatic in production
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
