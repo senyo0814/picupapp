942fcb8eb4e2be37c30810613d89141b7df5f905
senyo0814 <chingescat@gmail.com>
2025-05-07 17:12:18 +0000

    add google cloud storage

--- mysite/storage_backends.py
+++ mysite/storage_backends.py
@@ -1,10 +1,17 @@
-# picupapp/storage_backends.py
-
 from storages.backends.gcloud import GoogleCloudStorage
-from django.conf import settings
+from google.oauth2 import service_account
+import os
 
 class CustomGoogleCloudStorage(GoogleCloudStorage):
     def __init__(self, *args, **kwargs):
-        kwargs['bucket_name'] = settings.GS_BUCKET_NAME
-        kwargs['credentials'] = settings.GS_CREDENTIALS
-        super().__init__(*args, **kwargs)
+        # Load bucket name from environment variable
+        bucket_name = os.getenv('GS_BUCKET_NAME')
+
+        # Load credentials from the service account file if specified
+        credentials = None
+        creds_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+        if creds_path:
+            credentials = service_account.Credentials.from_service_account_file(creds_path)
+
+        # Initialize the GoogleCloudStorage backend with custom bucket and credentials
+        super().__init__(*args, **kwargs, bucket_name=bucket_name, credentials=credentials)
