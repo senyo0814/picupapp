8f239e17b5a83c1d44d38601b58cb86c37d64474
ching escat <chingescat@gmail.com>
2025-05-09 14:16:52 -0700

    update change password

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
