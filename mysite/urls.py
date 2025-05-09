e6cd91c98960d3785ed2071ffeebc33c6b2b2460
ching escat <chingescat@gmail.com>
2025-05-09 13:30:07 -0700

    add change password

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,19 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('picupapp.urls', 'picupapp'), namespace='picupapp')),
+    path('change-password/', auth_views.PasswordChangeView.as_view(
+        template_name='registration/change_password.html',
+        success_url='/change-password-done/'
+    ), name='change_password'),
+
+    path('change-password-done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='registration/change_password_done.html'
+    ), name='password_change_done'),
 ]
 
 # Serve media and static files in development
