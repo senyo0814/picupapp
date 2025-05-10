acdc2cd8046b4b6fde6579603da2f5dec368d44e
ching escat <chingescat@gmail.com>
2025-05-09 18:58:31 -0700

    fix

--- picupapp/views.py
+++ picupapp/views.py
@@ -167,6 +167,7 @@ def landing(request):
                     latitude=lat,
                     longitude=lon,
                     photo_taken_date=taken_date
+                    is_public=request.POST.get('visibility') == 'public'
                 )
 
                 # Explicitly save the file to trigger `upload_to=user_directory_path`
@@ -179,17 +180,17 @@ def landing(request):
                 if shared_ids:
                     photo.shared_with.set(User.objects.filter(id__in=shared_ids))
 
-                return redirect('picupapp:landing')
+            return redirect('picupapp:landing')
 
-                return render(request, 'picupapp/landing.html', {
-                    'photos': valid_photos,
-                    'username': request.user.username,
-                    'all_users': User.objects.exclude(id=request.user.id)
-                })
+        return render(request, 'picupapp/landing.html', {
+            'photos': valid_photos,
+            'username': request.user.username,
+            'all_users': User.objects.exclude(id=request.user.id)
+        })
 
-                except Exception as e:
-                    logger.exception("Landing view error:")
-                    return HttpResponse("Something went wrong.", status=500)
+    except Exception as e:
+        logger.exception("Landing view error:")
+        return HttpResponse("Something went wrong.", status=500)
 
 # --- Delete Photo ---
 
